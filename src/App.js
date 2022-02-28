411ad072c80bf7b2da740aa8531e68d404d26e1c
Kth <vcro12123@naver.com>
2022-02-28 20:25:31 +0900

    :sparkles:  feat : RankingEtcBox 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import React from "react";
-import Home from "./pages/Home";
-import styled from "styled-components";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RankingEtc from './RankingEtc';
+import React from 'react';
+import Home from './pages/Home';
+import styled from 'styled-components';
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
       {/* 여기에 네비게이션 두시면 됩니다. */}
       <MainContainer>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path='/' element={<RankingEtc></RankingEtc>} />
         </Routes>
       </MainContainer>
     </BrowserRouter>
@@ -18,4 +18,3 @@ const App = () => {
 };
 const MainContainer = styled.main``;
 export default App;
-
