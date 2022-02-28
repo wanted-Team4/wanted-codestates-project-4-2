7ef0dc0ac5c622238348d4ee35ac681b701590b7
sareum <rkdtkfma@gmail.com>
2022-02-28 15:58:28 +0900

    :tada: Start : 프로젝트 시작

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import React from "react";
+import Home from "./pages/Home";
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      {/* 여기에 네비게이션 두시면 됩니다. */}
+      <MainContainer>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </MainContainer>
+    </BrowserRouter>
+  );
+};
+const MainContainer = styled.main``;
+export default App;
+
